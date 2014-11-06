12a036335a809e32126806d9fec80f930af2c711
jjbandit <revelstokesnowboard@gmail.com>
2014-11-06 13:51:49 -0800

    Button Working as intended

--- app/overrides/add_bulk_generate_to_product_index.rb
+++ app/overrides/add_bulk_generate_to_product_index.rb
@@ -2,7 +2,7 @@ Deface::Override.new(:virtual_path  => 'spree/admin/products/index',
 										 :name          => 'add_bulk_generate_to_product_index',
 										 :insert_bottom => 'li#new_product_link',
 										 :text          => '
-<li id="generate_new_products_link">
+<li id="new_batch_link">
 	<%= button_link_to Spree.t(:new_batch), \'products/batch\', { :remote => true, :icon => \'plus\', :id =>\'admin_new_batch\' } %>
 </li>
 
