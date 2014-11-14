7ed27b432b13478d4d8378828cfff0b47aafb901
jjbandit <revelstokesnowboard@gmail.com>
2014-11-13 21:52:14 -0800

    Image aspect ratio tweaks, CSS tweaks

--- app/controllers/spree/admin/image_decorator.rb
+++ app/controllers/spree/admin/image_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Image.class_eval do 
 	attachment_definitions[:attachment][:styles] = {
-		 mini: '150x150>', small: '300x400#', product: '280x440#', large: '400x800#'
+		 mini: '150x150#', small: '300x400#', product: '320x440#', large: '400x800#'
 	}
 
 end
