5ae523d130bff92f26c6b02ce140ddae6e4c9f86
jjbandit <revelstokesnowboard@gmail.com>
2014-11-14 23:40:49 -0800

    for andy, with love

--- app/controllers/spree/admin/image_controller_decorator.rb
+++ app/controllers/spree/admin/image_controller_decorator.rb
@@ -1,11 +1,11 @@
 Spree::Api::ImagesController.class_eval do
 
-	def create
-		authorize! :create, Image
-		@image = scope.images.create(image_params.merge({:stuff => "things"}))
+##def create
+##	authorize! :create, Image
+##	@image = scope.images.create(image_params.merge({:stuff => "things"}))
 
-		respond_with(@image, :status => 201, :default_template => :show)
-	end
+##	respond_with(@image, :status => 201, :default_template => :show)
+##end
 
 end
 
--- app/controllers/spree/admin/image_decorator.rb
+++ app/controllers/spree/admin/image_decorator.rb
@@ -1,9 +1,17 @@
 Spree::Image.class_eval do 
 
-	attachment_definitions[:attachment][:styles] = lambda {|a| puts a.instance.mini_url} 
-#{
-#		mini: '150x150#', small: '300x400#', product: '320x440#', large: '400x700#'
-#	}
 
+	attachment_definitions[:attachment][:styles] = lambda do |a| 
+		
+	 	product_id = a.instance.viewable.product_id
+		product = Spree::Product.find(product_id)
+		taxons = product.taxon_ids
+		
+		if taxons.include? 5	
+			return {mini: '150x150#', small: '300x400#', product: '320x440#', large: '400x700#'}
+		else
+			return {mini: '150x150#', small: '400x300#', product: '440x320#', large: '400x700#'}
+		end
 
+	end
 end
