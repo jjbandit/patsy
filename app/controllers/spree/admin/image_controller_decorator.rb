17ec8b6e1919522e2c5835884300eb3cd448ae61
jjbandit <revelstokesnowboard@gmail.com>
2014-11-27 16:47:20 -0800

    Refactor erb conditional on images to render partials

--- app/controllers/spree/admin/image_controller_decorator.rb
+++ app/controllers/spree/admin/image_controller_decorator.rb
@@ -1,31 +1,3 @@
 Spree::Admin::ImagesController.class_eval do
 
-  def create
-		
-		puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1st'
-    invoke_callbacks(:create, :before)
-		puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!2nd'
-    @object.attributes = permitted_resource_params
-		puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!3rd'
-		puts @object.attributes
-		puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!4th'
-    if @object.save
-      invoke_callbacks(:create, :after)
-      flash[:success] = flash_message_for(@object, :successfully_created)
-      respond_with(@object) do |format|
-        format.html { redirect_to location_after_save }
-        format.js   { render :layout => false }
-      end
-    else
-      invoke_callbacks(:create, :fails)
-      respond_with(@object) do |format|
-        format.html do
-          flash.now[:error] = @object.errors.full_messages.join(", ")
-          render action: 'new'
-        end
-        format.js { render layout: false }
-      end
-    end
-  end
-
 end
