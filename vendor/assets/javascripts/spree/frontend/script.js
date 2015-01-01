88bc370d03e639cca51107c0aaf288caa54719bc
jjbandit <revelstokesnowboard@gmail.com>
2015-01-01 11:18:06 -0800

    add js to lander, css tweaks

--- vendor/assets/javascripts/spree/frontend/script.js
+++ vendor/assets/javascripts/spree/frontend/script.js
@@ -1,4 +1,13 @@
-$( window ).load(function() {
-	$("#products").hide();
-	$("#products").fadeIn();
+$( document ).ready(function() {
+
+	// Get current background image so we can animate it in
+	// var element = document.getElementById("default"),
+	// 	style = window.getComputedStyle(element),
+	// 	background = style.getPropertyValue("background"); 
+
+	$("#default").hide(0);
+	$("#default").fadeIn(800);
+	$("#products").hide(0);
+	$("#products").fadeIn(800);
+
 });
