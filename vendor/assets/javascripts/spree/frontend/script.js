8f0c42447381e906f7283aca553f9dc3cb4ddb12
jjbandit <revelstokesnowboard@gmail.com>
2015-01-01 22:50:55 -0800

    tweak jquery timings, set search bar margin:0

--- vendor/assets/javascripts/spree/frontend/script.js
+++ vendor/assets/javascripts/spree/frontend/script.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
 	
 	$(".container").hide();
-	$(".container").fadeIn(700);
+	$(".container").delay(150).fadeIn(550);
 
 	$("#products").children().each( function (index) {
 		$(this).hide(0);
-		$(this).delay(150*index + 75).fadeIn(700);
+		$(this).delay(150*index + 75).fadeIn(550);
 	});
 
 });
